11860494bdb3e28cb7909c253d268ae36119f3c0
Sara-Ferrer <saraferrer@gmail.com>
2021-02-26 17:40:04 +0100

    Intro-component-Sara

---
+++ README.md
@@ -0,0 +1,3 @@
+# intro-component
+ CDDR-Course Project 1. An introductory component with a sign-up form
+ 
---
+++ script.js
@@ -0,0 +1,64 @@
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+
+	// the trim is to remove the whitespaces
+	const firstNameValue = firstName.value.trim(); 
+    const lastNameValue = lastName.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+
+	
+	if(firstNameValue === '') {
+		setErrorFor(firstName, 'First Name cannot be blank');
+	} else {
+		setSuccessFor(firstName);
+	}
+
+    if(lastNameValue === '') {
+		setErrorFor(lastName, 'Last Name cannot be blank');
+	} else {
+		setSuccessFor(lastName);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+	
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
